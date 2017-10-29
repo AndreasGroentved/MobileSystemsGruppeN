8fd5d6b9e3508844f36bac6c37a4ebd3816279b1
Andreas Grøntved <andreasgro@gmail.com>
2017-10-29 17:29:28 +0100

    Initial commit

---
+++ domain/src/main/java/dk/sdu/gruppen/domain/test/Tester.java
@@ -0,0 +1,16 @@
+package dk.sdu.gruppen.domain.test;
+
+/**
+ * Created by Andreas Grøntved on 29-10-2017.
+ **/
+
+public class Tester {
+    private int test1 = 1;
+
+
+    public Tester(){
+
+    }
+
+
+}
