c0facce8d09159d22211c6879207b48d90635c19
Andreas Grøntved <andreasgro@gmail.com>
2017-11-10 15:03:04 +0100

    Test

--- presentation/src/main/java/dk/sdu/gruppen/mobilesystems/main/MainActivity.java
+++ presentation/src/main/java/dk/sdu/gruppen/mobilesystems/main/MainActivity.java
@@ -3,22 +3,30 @@ package dk.sdu.gruppen.mobilesystems.main;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import dk.sdu.gruppen.mobilesystems.R;
 
 public class MainActivity extends AppCompatActivity {
 
     private MainViewModel viewModel;
 
+    @BindView(R.id.example)
+    TextView example;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
 
-
+        viewModel.getExample().observe(this, s -> {
+            example.setText(s);
+        });
 
     }
 }
